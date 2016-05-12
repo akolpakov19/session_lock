7c49e7def730af9f6e77b561e57406f86ac49db5
akolpakov <akolpakov@gorillagroup.com>
2016-05-12 17:48:54 +0200

    changing locker

--- locker.php
+++ locker.php
@@ -7,8 +7,13 @@
  */
 require_once "session_config.php";
 require_once "memcached_config.php";
+require_once "output.php";
 
 session_id(SESSION_NAME);
+$sleep_time = 10;
+//output('Starting session');
 session_start();
 $_SESSION['test'] = 1;
-sleep(1000);
+output("Sleeping...");
+sleep($sleep_time);
+output("Checking the session:".$_SESSION['test']);
